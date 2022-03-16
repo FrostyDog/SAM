753b59dee176400a0a522564eccd09b55e42b0c4
root <root@pop-os.localdomain>
2022-03-16 02:25:43 +0100

    feat: v 1.0 - working base

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"strconv"
 	"time"
+
+	"github.com/FrostyDog/SAM/utility"
 
 	"github.com/Kucoin/kucoin-go-sdk"
 	"github.com/google/uuid"
@@ -24,7 +27,7 @@ var currentPrice string
 var targetOperation string
 var targetPrice string
 
-var transactionExist bool = false
+var transactionNotExists bool = false
 var nextOperation string = "sell"
 
 var numberOfTransaction = 0
@@ -45,10 +48,10 @@ func main() {
 func checkOrder(s *kucoin.ApiService) {
 	var params = map[string]string{
 		"tradeType": "TRADE",
-		"staus":     "active",
+		"status":    "active",
 	}
 
-	var paginationParam = kucoin.PaginationParam{PageSize: 10, CurrentPage: 0}
+	var paginationParam = kucoin.PaginationParam{PageSize: 10, CurrentPage: 1}
 
 	resp, err := s.Orders(params, &paginationParam)
 	if err != nil {
@@ -56,11 +59,12 @@ func checkOrder(s *kucoin.ApiService) {
 	}
 
 	as := kucoin.OrdersModel{}
-	resp.ReadData(&as)
+	_, err = resp.ReadPaginationData(&as) // put variable instead of blank to see pagination/page resutlss
+	if err != nil {
+		fmt.Println("Failed at reading pagination")
+	}
 
-	transactionExist = len(as) != 0
-
-	log.Printf("length of the orders: %d", len(as) == 0)
+	transactionNotExists = len(as) == 0
 }
 
 func launchTicker(s *kucoin.ApiService) {
@@ -69,22 +73,22 @@ func launchTicker(s *kucoin.ApiService) {
 	for _ = range ticker.C {
 		getPrice(s, dSymbol)
 		checkOrder(s)
-		if !transactionExist {
+
+		if transactionNotExists {
 			calculatePrice(nextOperation)
 
 			if nextOperation == "sell" {
 				sellCoin(s, "", targetPrice)
-			}
-
-			if nextOperation == "buy" {
+			} else {
 				buyCoin(s, "", targetPrice)
 			}
 
 			numberOfTransaction++
 		}
 
-		if numberOfTransaction == 5 {
+		if numberOfTransaction == 10 {
 			ticker.Stop()
+			os.Exit(0)
 		}
 	}
 
@@ -97,7 +101,7 @@ func calculatePrice(side string) {
 			fmt.Println("error accured during parsing")
 		}
 		var p float64 = t + t*0.003
-		targetPrice = fmt.Sprint(p)
+		targetPrice = fmt.Sprint(utility.RoundFloat(p, 5))
 	}
 
 	if side == "buy" {
@@ -105,8 +109,8 @@ func calculatePrice(side string) {
 		if err != nil {
 			fmt.Println("error accured during parsing")
 		}
-		var p float64 = t - t*0.003
-		targetPrice = fmt.Sprint(p)
+		var p float64 = t - t*0.0025
+		targetPrice = fmt.Sprint(utility.RoundFloat(p, 5))
 	}
 
 }
@@ -125,8 +129,6 @@ func getPrice(s *kucoin.ApiService, symbol string) {
 	}
 
 	currentPrice = as.Price
-
-	log.Printf("Token %s: with Price %s", as.BestAsk, as.Price)
 }
 
 func buyCoin(s *kucoin.ApiService, sy string, price string) {
@@ -140,6 +142,10 @@ func buyCoin(s *kucoin.ApiService, sy string, price string) {
 	o := kucoin.CreateOrderModel{ClientOid: uuid.New().String(), Side: "buy", Symbol: sy, Price: price, Size: size}
 
 	_, err := s.CreateOrder(&o)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if err == nil {
 		fmt.Println("buy operation done")
@@ -160,8 +166,11 @@ func sellCoin(s *kucoin.ApiService, sy string, price string) {
 
 	_, err := s.CreateOrder(&o)
 
-	if err == nil {
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	if err == nil {
 		fmt.Println("sell operation done")
 		nextOperation = "buy"
 	}
